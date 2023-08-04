b906de83db5b189b1d4165954a20724a0cf372dd
chrissy3815 <cmh2175@columbia.edu>
2023-08-04 15:40:26 -0400

    minimum viable IPM

---
+++ code/suckers_oldcode.R
@@ -0,0 +1,16 @@
+## Old code snippets that I don't want to delete yet.
+
+# Ideally, we'd want to use a von Bertalanffy growth model with an Lmax much
+# bigger than the largest observed fish, and linear growth in the observed
+# range.
+
+# estimating Linf from the Ford-Walford method:
+meanLAA<- aggregate(Len~age, data=femaleLH, FUN=mean)
+Lt<- meanLAA$Len[1:(length(meanLAA$Len)-1)]
+Lt1<- meanLAA$Len[2:length(meanLAA$Len)]
+plot(Lt, Lt1, xlim=c(100, 700), ylim=c(100, 700))
+Linf_model<- lm(Lt1~Lt)
+abline(61.4445, 0.8665)
+abline(0,1, lty=2)
+# estimate K is -ln(slope) of the linear model:
+K<- -log(0.8665)
--- examples/gizzard_shad-main/Rcode/gizshad_parameter_estimation.R
+++ examples/gizzard_shad-main/Rcode/gizshad_parameter_estimation.R
@@ -4,16 +4,16 @@
 
 # I.EGG PRODUCTION
 # Using the batch fecundity vs length (Figure 1a) from Jons & Miranda (1997)
-egg_size_data <- read.csv('./EggSizeData.csv',header=TRUE,sep=",")
+egg_size_data <- read.csv('examples/gizzard_shad-main/Rcode/EggSizeData.csv',header=TRUE,sep=",")
 # assume zero eggs below lowest length in data
-zero_eggs <- data.frame(x = seq(floor(min(egg_size_data$x))), 
+zero_eggs <- data.frame(x = seq(floor(min(egg_size_data$x))),
                         EggLengthData=rep(0,floor(min(egg_size_data$x))))
 # assume max eggs for length greater than recorded in data
-max_eggs <-  data.frame(x = seq(ceiling(max(egg_size_data$x)), 500), 
+max_eggs <-  data.frame(x = seq(ceiling(max(egg_size_data$x)), 500),
                         EggLengthData=max(egg_size_data$EggLengthData))
 egg_size_data_ext <- rbind(zero_eggs, egg_size_data, max_eggs)
 
-### We fit a 3 parameter (max, slope, inflection) logit model to the eggs produced data 
+### We fit a 3 parameter (max, slope, inflection) logit model to the eggs produced data
 # since biological observations suggest that eggs are not produced by females below size 140mm.
 egg_extended_nls <- nls(EggLengthData~egg_max/(1+exp(egg_slope*(log(x)-log(egg_inf)))),
                         data = egg_size_data_ext,
@@ -28,6 +28,6 @@ egg_extended_nls <- nls(EggLengthData~egg_max/(1+exp(egg_slope*(log(x)-log(egg_i
 # import data from Michaletz paper
 Michaletz_data <- read.csv('./Michaletz2009.csv',header=TRUE,sep=",")
 names(Michaletz_data) <- c('density', 'survival')
-surv_den_exp <- nls(survival/100~s0*exp(-b*density), 
+surv_den_exp <- nls(survival/100~s0*exp(-b*density),
                     data = Michaletz_data,
                     start = list(s0=0.3, b=0.01))
