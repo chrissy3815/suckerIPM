5f3368339e1fc1415530b3266d41021b3c53517a
chrissy3815 <cmh2175@columbia.edu>
2023-08-04 15:40:26 -0400

    minimum viable IPM

--- code/suckers_modeling.R
+++ code/suckers_modeling.R
@@ -5,16 +5,20 @@
 ###########################################################################
 # Load necessary packages:
 library(readxl)
-
-# Set working directory:
-setwd("/Users/chrissy/Cornell_Postdoc/suckers_modeling/")
+library(here)
+source(here("code", "MatrixImage.R"))
 
 # Read in data:
-observations<- read_xlsx("sucker_model_data.xlsx")
-lifehistory<- read_xlsx("life_history_model.xlsx")
+observations<- read_xlsx(here("data", "sucker_model_data.xlsx"))
+lifehistory<- read_xlsx(here("data", "life_history_model.xlsx"))
 # get rid of blank rows in observations table:
 I<- which(is.na(observations$year))
 observations<- observations[-I,]
+# convert fecundity to numeric:
+lifehistory$fecundity<- as.numeric(lifehistory$fecundity)
+# fix the mislabeled male who produced eggs:
+I<- which(lifehistory$sex=="M" & lifehistory$fecundity>0)
+lifehistory$sex[I]<- "F"
 # pull out females only:
 females<- observations[observations$sex=="F",]
 femaleLH<- lifehistory[lifehistory$sex=="F",]
@@ -41,65 +45,140 @@ summary(lineargrowth)
 par(mfrow=c(1,1))
 plot(femaleLH$age, femaleLH$Len)
 
-# Ideally, we'd want to use a von Bertalanffy growth model with an Lmax much
-# bigger than the largest observed fish, and linear growth in the observed
-# range.
-
-# estimating Linf from the Ford-Walford method:
-meanLAA<- aggregate(Len~age, data=femaleLH, FUN=mean)
-Lt<- meanLAA$Len[1:(length(meanLAA$Len)-1)]
-Lt1<- meanLAA$Len[2:length(meanLAA$Len)]
-plot(Lt, Lt1, xlim=c(100, 700), ylim=c(100, 700))
-Linf_model<- lm(Lt1~Lt)
-abline(61.4445, 0.8665)
-abline(0,1, lty=2)
-# estimates:
-# Linf is the point where the linear model and the 1:1 line intersect:
-Linf<- 61.4445/(1-0.8665)
-# K is -ln(slope) of the linear model:
-K<- -log(0.8665)
-# t0 can be estimated from one of the age-meanlength pairs:
-I<- which(meanLAA$age==14)
-t0<- meanLAA$age[I]*log((Linf-meanLAA$Len[I])/Linf)
-
-vbStarts<- list(Linf=Linf, K=K, t0=t0)
+# Take Lmax from the literature: 500 mm, and this is reasonable for the dataset because the largest individual was 465
+vbStarts<- list(Linf=500, K=0.1, t0=-3)
 vbTypical<-Len~Linf*(1-exp(-K*(age-t0)))
-
-fitTypical<-nls(vbTypical,data=femaleLH[,c("age", "Len")],start=vbStarts)
+data_forfitting<- femaleLH[,c("age", "Len")]
+data_forfitting$Linf<- vbStarts$Linf
+#data_forfitting$K<- vbStarts$K
+fitTypical<-nls(vbTypical,data=data_forfitting, start=list(K=vbStarts$K, t0=-3))
 
 plot(femaleLH$age, femaleLH$Len, xlim=c(1,20), ylim=c(100, 600))
 exes<- 1:20
-whys<- Linf*(1-exp(-K*(exes-t0)))
+whys<- vbStarts$Linf*(1-exp(-coef(fitTypical)[1]*(exes-coef(fitTypical)[2])))
 lines(exes, whys)
 
+# sd about mean: Pierce et al. say that they use max(L_obs)-Linf, but I can't make it make sense.
+grow_sd<- 25 #abs(max(femaleLH$Len)-500)
+growth_params<- list(Linf=500, K=coef(fitTypical)[1], t0=coef(fitTypical)[2],
+                     grow_sd = grow_sd)
 
-m_par <- tibble(
-  ## growth from Michaletz (2017) paper - Table 1
-  grow_rate = 0.26, # growth rate
-  grow_max  =   394.3, # maximum length in mm: L_inf = 394.30
-  grow_sd   =   25,  # growth sd (Max TL - L_inf)
-  surv_min  =  0.002, # min survival - Bodola (1955)
-  # Then et al (2015): surv_max computed from 
+###########################################################################
+### EGG PRODUCTION model
+###########################################################################
+# linear model in log-log space (Carl's results)
+egg_model<- lm(formula = log(fecundity) ~ log(Len), data=femaleLH)
+
+###########################################################################
+### Mostly following Pierce et al. 2023, model building:
+###########################################################################
+
+# assign parameters:
+m_par <- list(
+  ## Growth parameters
+  grow_rate = growth_params$K, # growth rate
+  Linf  =   growth_params$Linf, # maximum length in mm
+  grow_sd   =   growth_params$grow_sd,  # growth sd
+  ## Survival parameters
+  surv_min  =  0.002, # PLACEHOLDER min survival - gizzard shad Bodola (1955)
+  # Then et al (2015): surv_max computed from
   # 1-natural mortality = 1 - 8.872*K^.73 L^-.33
-  surv_max = 1 - 8.872*grow_rate^.73*grow_max^(-.33), 
+  surv_max = 1 - 8.872*growth_params$K^.73*growth_params$Linf^(-.33),
   # inflection point: will be temp dependent
   # computed for La Grange Reach
-  surv_alpha = 80.0136, 
-  surv_beta = -139.9312, # slope
-  ## New recruit from Michaletz (2017)
-  recruit_mean = 105,
-  recruit_sd = 25, # same as grow_sd
-  ## From Bodola (1955):
-  egg_viable = 0.002,
-  ## Estimated from Jons and Miranda (1997)
-  egg_slope = coef(egg_extended_nls)[2], # -4.361915
-  egg_max =  coef(egg_extended_nls)[1], # 41540.608025
-  egg_infl = coef(egg_extended_nls)[3], # 935.528239
-  ## Spawning Probability - Estimated from Michaletz (2009)
-  prob_spawn = 0.90,
-  surv0_int = coef(surv_den_exp)[1], # 0.2686
-  surv0_decay = coef(surv_den_exp)[2], # 0.0030
+  surv_alpha = 80.0136, # PLACEHOLDER - taken from Pierce et al. 2023
+  surv_beta = -139.9312, # slope PLACEHOLDER - taken from Pierce
+  ## Size of age-1 individuals:
+  recruit_mean = 112, # mean size of age-1 individuals
+  recruit_sd = growth_params$grow_sd, # same as grow_sd
+  ## PLACEHOLDER:
+  egg_viable = 0.02,
+  ## Estimated from fecundity data
+  egg_logslope = egg_model$coefficients[2], # 2.776441
+  egg_logintercept =  egg_model$coefficients[1], # -7.988805
+  ## Spawning Probability
+  prob_spawn = 0.90, # PLACEHOLDER
+  ## YOY survival probability:
+  s0= 0.01 # PLACEHOLDER
 )
 
+##########################
+## Section 2: Model Set-up
+##########################
 
+## Growth function
+# given you are size z now returns the pdf of size z1 next time
+# computed from von Bertanaffy equation z(t) = L_inf(1-e^K(t-t0))
+# to find z(t+1) = L_inf*(1-e^(-K)) + e^(-K)*z(t)
 
+g_z1z <- function(z1, z, m_par) {
+  mu <- m_par$Linf * (1 - exp(- m_par$grow_rate)) +
+    exp(-m_par$grow_rate) * z           # mean size next year
+  sig <- m_par$grow_sd                       # sd about mean
+  p_den_grow <- dnorm(z1, mean = mu, sd = sig)    # pdf that you are size z1
+  # given you were size z
+  return(p_den_grow)
+}
+
+## Adult Survival function, 4-parameter logistic
+s_z <- function(z, m_par) {
+  m_par$surv_min + (m_par$surv_max - m_par$surv_min) /
+    (1 + exp(m_par$surv_beta * (log(z) - log(m_par$surv_alpha))))
+}
+
+## Reproduction, log-linear
+eggs_z <- function(z, m_par) { # Eggs produced (note: data are in thousands)
+  eggz<- exp(m_par$egg_logslope*log(z) + m_par$egg_logintercept)
+  return(eggz)
+}
+
+## Recruit size pdf
+c_1z1 <- function(z1, m_par) {
+  mu <- m_par$recruit_mean
+  sig <- m_par$recruit_sd
+  p_den_recruit <- dnorm(z1, mean = mu, sd = sig)
+  return(p_den_recruit)
+}
+
+#####################################################
+## Section 3 - Build IPM kernels F and P
+#####################################################
+
+## Fecundity Kernel
+f_z1z <- function(z1, z, m_par) {
+  age1_dist <- m_par$prob_spawn * eggs_z(z, m_par) *
+    m_par$egg_viable * m_par$s0
+  #returns fecundity kernel (as a matrix). Recruits= F.dot(n*delta_z)
+  return(outer(c_1z1(z1, m_par), age1_dist))
+}
+
+## Growth and Survival Kernel
+p_z1z <- function(z1, z, m_par) {
+  N<- length(z1)
+  delta_z<- z1[2]-z1[1]
+  g_matrix <- matrix(0, N, N)
+  for (x in 1:N) {
+    g_matrix[, x] <- g_z1z(z, rep(z[x], times = N), m_par)
+    g_matrix[, x] <- g_matrix[, x] / (sum(g_matrix[, x]) * delta_z)
+  }
+  return(g_matrix %*% diag(s_z(z, m_par)))
+}
+
+## Build the deterministic kernels ##
+m <- 300 # number of meshpoints: bins for the integration
+L <- 0.00   # lower size limit in mm
+U <- 600.00    # upper size limit in mm - must larger than Linf
+h <- (U-L)/m # integration bin width
+meshpts <-  L + (1:m)*h - h/2
+
+Pmat<- h*(p_z1z(meshpts, meshpts, m_par))
+Fmat<- h*(f_z1z(meshpts, meshpts, m_par))
+Kmat<- Pmat+Fmat
+
+# Plot the kernels to check it looks okay
+matrix.image(Pmat, x=meshpts, y=meshpts, main='Growth+Survival')
+matrix.image(Fmat, x=meshpts, y=meshpts, main='Reproduction')
+matrix.image(Kmat, x=meshpts, y=meshpts, main='Projection Kernel')
+
+# population growth rate (currently, it shows the population as just barely growing
+max(Re(eigen(Kmat, only.values=TRUE)$values))
