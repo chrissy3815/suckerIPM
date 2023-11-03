36431e9a3a1efcd2f2afe711f502af8c313de070
Chrissy Hernandez <cmh352@cornell.edu>
2023-11-03 16:10:06 +0000

    add code to plot the survival probability at size

--- code/suckers_CheckPerformance.R
+++ code/suckers_CheckPerformance.R
@@ -25,17 +25,35 @@ whys<- exp(m_par$egg_logslope*log(exes) + m_par$egg_logintercept)
 #whys<- exp(2.776441*log(exes) - 7.988805)
 lines(exes, whys)
 
+# eigenvalues and eigenvectors:
+eigz<- eigen(Kmat)
+# population growth rate
+lambda<- max(Re(eigz$values))
+
+# calculate average lifespan:
+lifespan(Pmat) # note that this is dependent on starting size.
+# Small individuals (including YOY) live only one year on average.
+# Larger individuals have an expectation of more years of life.
+
+# plot the stable size distribution
+popvec<- eigz$vectors[,1]
+popvec<- Re(popvec)/sum(Re(popvec))
+par(mfrow=c(1,1))
+plot(meshpts, popvec, type='l', xlab='Length (mm)', ylab='Probability density')
+
 # plot the stable size distribution without any YOY:
 ws_dist_noYOY<- stable_size_dist_noYOY(Pmat, Fmat)
 plot(meshpts, ws_dist_noYOY, type='l', xlab='Length (mm)', ylab='Probability density')
 
 # plot the stable size distribution, assuming no individuals under 80 mm caught:
 ws_dist_noCatch80mm<- stable_size_dist_sizeThreshold(Pmat, Fmat, meshpts, threshold=80)
-plot(meshpts, ws_dist_noCatch100mm, type='l', xlab='Length(mm)', ylab='Probability density')
+plot(meshpts, ws_dist_noCatch100mm, type='l', xlab='Length (mm)', ylab='Probability density')
 
 # calculate the age at which 95% of individuals are dead:
 age_most_individuals_dead(Pmat, Fmat, proportion=0.95)
 # calculate the age at which 99% of individuals are dead:
 age_most_individuals_dead(Pmat, Fmat, proportion=0.99)
 
-
+# Plot the survival-at-size curve from the kernel:
+surv_at_size<- colSums(Pmat)
+plot(meshpts, surv_at_size, type='l', xlab='Length (mm)', ylab='Probability of survival to t+1')
